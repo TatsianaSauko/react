1a96143f8697ba088c13cf55c7ec9606ada637f2
TatsianaSauko <miljanzowa@mail.ru>
2023-10-29 13:04:47 +0300

    feat: add buttunerror

---
+++ src/components/ButtonError.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ButtonErrorProps {
+  clickButtonError: () => void;
+}
+
+class ButtonError extends React.Component<ButtonErrorProps> {
+  constructor(props: ButtonErrorProps) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <button className="button__error" onClick={this.props.clickButtonError}>
+        Error
+      </button>
+    );
+  }
+}
+
+export default ButtonError;
